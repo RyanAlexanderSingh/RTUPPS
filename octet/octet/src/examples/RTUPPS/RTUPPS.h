487bdaaa5a4924c3be3c97723e0c258a84bf17e6
Ryan MacLaptop <ryan2229@hotmail.co.uk>
2015-03-06 15:51:02 +0000

    adding particle struct in RTUPPS.h

--- octet/octet/src/examples/RTUPPS/RTUPPS.h
+++ octet/octet/src/examples/RTUPPS/RTUPPS.h
@@ -9,14 +9,23 @@ namespace octet {
   class RTUPPS : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+
+    struct Particle{
+      vec3 position;
+      vec3 velocity;
+      float invmass;
+      uint32_t phase;
+    };
+
   public:
     /// this is called when we construct the class before everything is initialised.
     RTUPPS(int argc, char **argv) : app(argc, argv) {
     }
 
+
     /// this is called once OpenGL is initialized
     void app_init() {
-      app_scene =  new visual_scene();
+      app_scene = new visual_scene();
       app_scene->create_default_camera_and_lights();
 
       material *red = new material(vec4(1, 0, 0, 1));
@@ -33,7 +42,7 @@ namespace octet {
       app_scene->begin_render(vx, vy);
 
       // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+      app_scene->update(1.0f / 30);
 
       // draw the scene
       app_scene->render((float)vx / vy);
